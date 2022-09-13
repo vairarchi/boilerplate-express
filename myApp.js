cf3f539cd3565bf2a2e9543bd361c10b8b462a5c
Vaibhav Chichmalkar <vaibhav.chichmalkar@autodesk.com>
2022-09-13 22:18:15 +0530

    root level request logger middleware

--- myApp.js
+++ myApp.js
@@ -1,57 +1,27 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-require('dotenv').config()
+require("dotenv").config();
 
-console.log("Hello World")
+console.log("Hello World");
 
-// app.get("/", function(req, res) {
-//     res.send("Hello Express");
-//   });
+app.use(function (req, res, next) {
+  console.log(req.method, " ", req.path, " - ", req.ip);
+  next();
+});
 
-    absolutePath = __dirname + '/views/index.html';
-  app.get("/", function(req, res){
-    res.sendFile(absolutePath);
-  });
+absolutePath = __dirname + "/views/index.html";
+app.get("/", function (req, res) {
+  res.sendFile(absolutePath);
+});
 
-  app.use("/public", express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 
-  app.get("/json", function (req, res) {
+app.get("/json", function (req, res) {
   if (process.env.MESSAGE_STYLE == "uppercase") {
-    res.json({"message": "HELLO JSON"});
+    res.json({ message: "HELLO JSON" });
   } else {
-    res.json({"message": "Hello json"});
+    res.json({ message: "Hello json" });
   }
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
