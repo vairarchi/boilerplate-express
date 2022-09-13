735bffee795c658592f43a20cce4a56466c1bdff
Vaibhav Chichmalkar <vaibhav.chichmalkar@autodesk.com>
2022-09-13 21:57:34 +0530

    added env file

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 
 console.log("Hello World")
 
@@ -14,8 +15,12 @@ console.log("Hello World")
 
   app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/json", function (req, res) {
+  app.get("/json", function (req, res) {
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    res.json({"message": "HELLO JSON"});
+  } else {
     res.json({"message": "Hello json"});
+  }
 });
 
 
